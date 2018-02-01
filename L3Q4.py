71062993b88b89f6b41ae8ec681e09b6bda9524b
setom <matthew.y.seto@gmail.com>
2018-01-31 17:29:02 -0800

    Added Q4

---
+++ L3Q4.py
@@ -0,0 +1,44 @@
+import turtle
+import arcpy
+from arcpy import env
+env.workspace = "D:\Projects\ArcGIS Projects\GIS501_Lab3\RAW_DATA\Exercise07"
+#Overwrite files if they exist
+arcpy.env.overwriteOutput = True 
+
+
+def getSides() :
+    sides = input("How many sides do you want the turtle to draw?")
+    while sides < 3 :
+        sides = input("Polygons must have at least 3 sides! How many sides do you want to draw?")
+    return sides
+
+def getArray(sides) :
+    array = arcpy.Array()
+    point = arcpy.Point()
+    angle = (((sides - 2) * 180) / sides) - 180
+    wn = turtle.Screen()
+    turt = turtle.Turtle()
+    while sides > 0:
+        turt.left(angle)
+        turt.forward(50)
+        point.ID = sides
+        point.X = turt.xcor()
+        point.Y = turt.ycor()
+        array.add(point)
+        sides -= 1
+    return array
+    wn.exitonclick()
+    
+def makePolygon(array) :
+    sr = arcpy.SpatialReference(4326)
+    arcpy.CreateFeatureclass_management(env.workspace, "turtle_polygon.shp", "POLYGON", "", "", "", sr)
+    polygon = arcpy.Polygon(array)
+    cursor = arcpy.da.InsertCursor("turtle_polygon.shp", ["SHAPE@"])
+    cursor.insertRow([polygon])
+    del cursor
+    
+#Main
+sides = getSides()
+array = getArray(sides)
+makePolygon(array)
+
